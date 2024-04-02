919be048f71a2c6d04be4bd9ad474cb77ca6856b
Olaf Szyncel <101902323+olafszyncel@users.noreply.github.com>
2024-04-02 15:23:59 +0200

    Feat: style, login required decorators

--- calcal/static/calcal/app.js
+++ calcal/static/calcal/app.js
@@ -1,3 +1,4 @@
+//if user want to maintain his weight, this function disable few options
 function disableSlider() {
     const x = document.getElementById("goal-select").value;
     const rate = document.querySelector("#rate");
@@ -7,7 +8,6 @@ function disableSlider() {
         gw.style.display = 'none';
         rate.value = '0';
     } else {
-        
         rate.style.display = 'flex';
         gw.style.display = 'flex';
         rate.value = '100';
@@ -15,15 +15,20 @@ function disableSlider() {
     }
 }
 
+// function show current day menu
 function showSection(day) {
+    document.querySelectorAll('.btn-day').forEach(btn => {
+                btn.classList = "btn-day btn btn-outline-secondary";
+    })
+    document.querySelector(`#btn-day${day}`).classList = "btn-day btn btn-light";
     document.querySelector('#b-content').innerHTML = "";
     document.querySelector('#l-content').innerHTML = "";
     document.querySelector('#d-content').innerHTML = "";
     document.querySelector('#single-day').value = `${day}`;
+    // request for data
     fetch(`/weekday/${day}`)
     .then(response => response.json())
     .then(meals => {
-        console.log(meals)
         var counter = 0;
         var allCalories = 0;
         var allFat = 0;
@@ -41,7 +46,7 @@ function showSection(day) {
                 carbs = carbs + product.carbs*x;
                 protein = protein + product.protein*x;
                 const element = document.createElement('div');
-                element.innerHTML = `<div id="meal${counter}id${product.id}" class="card-body"><h5><b>${product.product}</b>   <i>${product.gram}g</i>
+                element.innerHTML = `<div id="meal${counter}id${product.id}" class="card-body single-product"><h5><b>${product.product}</b>   <i>${product.gram}g</i>
                 <button class="btn btn-outline-danger" type="submit" onclick="deleteProduct(${counter}, ${product.id}, ${day})">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
@@ -60,6 +65,7 @@ function showSection(day) {
                 
                 
             })
+            // print the main values of the meal 
             if (counter === 0) {
                 document.querySelector('#b-caloric-head').innerHTML = `${calories}kcal fat: ${fat}g carbs: ${carbs}g protein: ${protein}g`;
             } else if (counter === 1) {
@@ -72,48 +78,33 @@ function showSection(day) {
             allCarbs = allCarbs + carbs;
             allFat = allFat + fat;
             allProtein = allProtein + protein;
+            // print values of the whole day
             document.querySelector('#day-caloric-head').innerHTML = `${allCalories}kcal fat: ${allFat}g carbs: ${allCarbs}g protein: ${allProtein}g`
         });
     })
 }
 
-function showPage(page) {
-    document.querySelectorAll('.day').forEach(div => {
-        div.style.display = 'none';
-    });
-    document.querySelectorAll('.btn-day').forEach(btn => {
-                btn.classList = "btn-day btn btn-secondary";
-    })
-    document.querySelector(`#btn-${page}`).classList = "btn-day btn btn-light";
-    document.querySelector(`#${page}`).style.display = 'block';
-    
-}
 
 document.addEventListener('DOMContentLoaded', function() {
-
     document.querySelectorAll('.btn-day').forEach(button => {
         button.onclick = function() {
             const section = this.dataset.section;
-            
             showSection(section);
-            
-            showPage(this.dataset.page);
-            console.log(this.dataset.page);
         }
     })
 });
 
+// after page was realoaded, show current day
 function thisDay() {
     const d = new Date();
     let day = d.getDay()
     showSection(day);
     day = "day" + day;
-    showPage(day);
 }
-
 window.onload = thisDay;
 
+// function that deletes product
 function deleteProduct(meal, id, day) {
     fetch(`/delete/${meal}/${id}`)
-    showSection(day);
+    setTimeout(() => {showSection(day);}, 100)
 }
--- calcal/views.py
+++ calcal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.db import IntegrityError
 from django.views.decorators.csrf import csrf_exempt
@@ -11,12 +11,14 @@ from .models import User, Profile, Breakfast, Lunch, Dinner
 import datetime, json, requests
 
 
-
+# render main page
 def index(request):
     return render(request, "calcal/index.html")
-        
+
+@login_required(login_url="/login")
 def profile(request):
     user = User.objects.get(pk=request.user.id)
+    # section that get date from user input and save in the profile class
     if request.method == "POST":
         weight = float(request.POST["weight"])
         tall = int(request.POST["tall"])
@@ -42,6 +44,7 @@ def profile(request):
         rate = request.POST["rate"]
         goal_weight = request.POST["gw"]
 
+        # function that get current year
         year = int(datetime.date.today().year)
         age = year - birth
         if gender == "Female":
@@ -52,9 +55,11 @@ def profile(request):
             goal_weight = int(goal_weight)
 
         if goal_weight > weight:
-            caloric = caloric + (7700 * int(rate)/1000) / (1000 * 7)
+            caloric = round(caloric + (7700 * int(rate)/1000) / (1000 * 7))
         else:
-            caloric = caloric - (7700 * int(rate)/1000) / (1000 * 7)
+            caloric = round(caloric - (7700 * int(rate)/1000) / (1000 * 7))
+        
+        # if user already has profile just edit them
         try:
             profile = Profile.objects.get(user=user)
             profile.weight = weight
@@ -66,6 +71,7 @@ def profile(request):
             profile.rate = rate
             profile.caloric = caloric
         except:
+        # if user dont have profile yet, just create them
             profile = Profile(
                 user = user,
                 weight = weight,
@@ -83,7 +89,7 @@ def profile(request):
             "message": "Successfully save you profile details!"
         })
 
-    
+    # if request method is get, render profile html with user data if their exist
     try:
         profile = Profile.objects.get(user=user)
     except:
@@ -92,7 +98,25 @@ def profile(request):
         "profile": profile,
     })
 
+@login_required(login_url="/login")
 def menu(request):
+    try:
+        user = User.objects.get(pk=request.user.id)
+        profile = Profile.objects.get(user=user)
+        profile_weight = float(profile.weight)
+        # calculate macro needs
+        if profile.activity_lvl < 3:
+            profile_protein = [round(profile_weight * 0.8), profile_weight]
+        else:
+            profile_protein = [round(profile_weight * 1.2), profile_weight * 2]
+        profile_fat = [round(profile.caloric * 0.25 / 9), round(profile.caloric * 0.3 / 9)]
+        profile_carbs = [(profile.caloric - profile_protein[0]*4 - profile_fat[0]*9) / 4, (profile.caloric - profile_protein[1]*4 - profile_fat[1]*9) / 4]
+    except:
+        # if user wants to go to this section, but dont fill his profile render
+        return render(request, "calcal/profile.html", {
+        "message": "First, fill your profile details!",
+    })
+    # add meal to database
     if request.method == 'POST':
         query = request.POST["query"]
         gram = request.POST["amount"]
@@ -103,7 +127,7 @@ def menu(request):
         response = requests.get(api_url, headers={'X-Api-Key': '2o5Ie9y6Nn10L3cr3Tmbbg==IZ9m4PN7njKspjwh'})
         try:
             api = json.loads(response.content)
-            user = User.objects.get(pk=request.user.id)
+            
             match meal:
                 case "Breakfast":
                      breakfast = Breakfast(
@@ -141,19 +165,26 @@ def menu(request):
                         day = day
                      )
                     dinner.save()
-        except Exception as e:
-            api = "Something must go wrong, try again!"
+        except Exception:
             return render(request, "calcal/menu.html", {
-                "message": api
+                "message": "Something must go wrong, try again!"
             })
-        b = Breakfast.objects.filter(user=user, day=day)
-        l= Lunch.objects.filter(user=user, day=day)
-        d = Dinner.objects.filter(user=user, day=day)
-        return render(request, "calcal/menu.html")
-    # method is get
-    return render(request, "calcal/menu.html")
+        return render(request, "calcal/menu.html", {
+            "profile": profile,
+            "profile_protein": profile_protein,
+            "profile_carbs": profile_carbs,
+            "profile_fat": profile_fat
+        })
+    # if method is get
+    return render(request, "calcal/menu.html", {
+        "profile": profile,
+        "profile_protein": profile_protein,
+        "profile_carbs": profile_carbs,
+        "profile_fat": profile_fat
+    })
     
-
+# function that response data for user single day menu
+@login_required(login_url="/login")
 def weekday(request, day):
     user = User.objects.get(pk=request.user.id)
     b = Breakfast.objects.filter(user=user, day=day)
@@ -165,6 +196,8 @@ def weekday(request, day):
         [lunch.serialize() for lunch in l],
         [dinner.serialize() for dinner in d]], safe=False)
 
+# function delete menu data from data base
+@login_required(login_url="/login")
 def delete_product(request, meal, meal_id):
     match meal:
         case 0:
@@ -176,6 +209,8 @@ def delete_product(request, meal, meal_id):
     p.delete()
     return HttpResponseRedirect(reverse(menu))
 
+# function that creates list of items for shopping
+@login_required(login_url="/login")
 def shopping(request):
     user = User.objects.get(pk=request.user.id)
     b = Breakfast.objects.filter(user=user)
